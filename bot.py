f4392665023a8b1920b33ec619b056fb9a2aa6e2
Amar1729 <amar.paul16@gmail.com>
2022-01-30 18:40:21 -0500

    included pinning new msgs (and unpinning old msgs)

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ about - short info for this bot
 
 import datetime
 import logging
+import os
+from typing import Optional
 
 # third-party
 from telegram import Update
@@ -14,6 +16,8 @@ from telegram.ext import Updater, CallbackContext, CommandHandler
 # local
 from settings import TOKEN, TZ, GROUP_CHAT_ID
 
+
+MSG_FILE = "message.log"
 
 WORDLE_START = 225
 WORDLE_START_DATE = datetime.date(year=2022, month=1, day=30)
@@ -33,6 +37,19 @@ def about(update: Update, context: CallbackContext):
     )
 
 
+def get_recent_message() -> Optional[int]:
+    if os.path.exists(MSG_FILE):
+        with open(MSG_FILE, "r") as f:
+            return int(f.read().strip())
+
+    return None
+
+
+def save_message_id(message_id: int):
+    with open(MSG_FILE, "w") as f:
+        f.write(str(message_id))
+
+
 def wordle(context: CallbackContext):
     td = datetime.datetime.now().date() - WORDLE_START_DATE
     weekday = datetime.datetime.now().strftime("%A")
@@ -41,12 +58,19 @@ def wordle(context: CallbackContext):
 
     msg = f"{weekday} wordle: {current}"
 
-    context.bot.send_message(
+    sent_message = context.bot.send_message(
         chat_id=GROUP_CHAT_ID,
         text=msg,
     )
 
-    # how can i get the message.id of this message once it's sent?
+    if recent_msg := get_recent_message():
+        context.bot.unpin_chat_message(
+            chat_id=GROUP_CHAT_ID,
+            message_id=recent_msg,
+        )
+
+    sent_message.pin()
+    save_message_id(sent_message.message_id)
 
 
 if __name__ == "__main__":
